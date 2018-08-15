aa5d7f2a7cde6ffb71ec0f1f42a9876dcf396bb7
Siddique Data Engineer <dba.siddique@gmail.com>
2018-08-15 15:42:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+34 Clickstream Analytics - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 34-clickstream-analytics")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
